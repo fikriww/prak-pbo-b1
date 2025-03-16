e70009fe5344afc8b094e94656288fd37ade8174
Cipta Fikri Wiratama <91867817+fikriww@users.noreply.github.com>
2025-03-16 19:11:54 +0700

    Update DosenTamu.java

--- Pertemuan4umL/src/DosenTamu.java
+++ Pertemuan4umL/src/DosenTamu.java
@@ -1,7 +1,5 @@
 /* Nama File : DosenTamu.java
  * Deskripsi : Berisi atribut dan method dalam subclass Dosen Tamu
- * Pembuat : Gaza Al Ghozali Chansa / 24060123140183
- * Tanggal : Selasa, 12 Maret 2025
  */
 
  import java.time.LocalDate;
